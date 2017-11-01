4b49118ec183eafff37a519a768c190213aba658
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2017-11-01 14:38:02 +0500

    01/11/17

---
+++ js/common.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+//Slider
+    $('.slider').slick({
+        adaptiveHeight: true,
+        autoplay: true,
+    });
+//end Slider
+
+//Events filter
+    $('.future-events__item').click(function(){
+        $('.future-events__item').removeClass('future-events__item_active');
+        $(this).addClass('future-events__item_active');
+        $('.future-event').css('display', 'none');
+    });
+    $('.future-events__item_week').click(function(){
+        $('.future-event_week').css('display', 'block');
+    });
+    $('.future-events__item_month').click(function(){
+        $('.future-event_month').css('display', 'block');
+    });
+    $('.future-events__item_year').click(function(){
+        $('.future-event').css('display', 'block');
+    });
+//end Events filter
+//Calendar
+    $('.calendar-slider').slick({});
+//end Calendar
+//Gallery
+    $(window).on('load resize', function(){
+        var currentWidth = $('.gallery_item').outerWidth();
+        $(".gallery_item").height(currentWidth);
+    });
+    $('.gallery__link').magnificPopup({
+        type: 'image'
+    });
+//end Gallery
+});
